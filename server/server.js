248178c0bb4368b30a88096d9081f6692749d302
nagadarshan08 <darshanaravn.855rc@gmail.com>
2026-04-20 21:07:31 +0530

    Initial commit

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './configs/db.js';
-import { clerkMiddleware } from '@clerk/express'
-import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
 
-const app =  express();
-const port = 3000;
+const app = express();
 
-await connectDB();
+// Connect DB (no await here)
+connectDB().then(() => console.log("DB Connected"));
 
-app.use(express.json())
-app.use(cors())
-app.use(clerkMiddleware())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req,res)=> res.send('Server is live..'))
-app.use('/api/inngest', serve({ client: inngest, functions }))
+// Test route
+app.get("/", (req, res) => res.send("Server is live.."));
+
+// Inngest route
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 export default app;
