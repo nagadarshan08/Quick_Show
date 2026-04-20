a406456c6568d49efd4e4c5c6f3fd8a137afd85b
nagadarshan08 <darshanaravn.855rc@gmail.com>
2026-04-20 21:13:58 +0530

    fix vercel api structure

--- api/index.js
+++ api/index.js
@@ -1,5 +1,28 @@
-import app from "../server/server.js";
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
-export default function handler(req, res) {
-  return app(req, res);
-}
+import connectDB from "../server/configs/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "../server/inngest/index.js";
+
+const app = express();
+
+// connect DB
+await connectDB();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("API is working 🚀");
+});
+
+// Inngest route
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+export default app;
