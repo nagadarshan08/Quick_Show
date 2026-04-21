0a90c1254fe99455764c103f588d00a5f91a766a
nagadarshan08 <darshanaravn.855rc@gmail.com>
2026-04-21 23:03:47 +0530

    final working setup1

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI is not defined");
+    throw new Error("MONGODB_URI is not defined");
   }
 
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGODB_URI);
   console.log("MongoDB connected");
 };
 
