686b44c786e23ceb4920ec972e0a4a2405063df1
nagadarshan08 <darshanaravn.855rc@gmail.com>
2026-04-22 16:34:29 +0530

    fix inngest route properly

--- api/inngest.js
+++ api/inngest.js
@@ -2,7 +2,6 @@ import { serve } from "inngest/next";
 import { inngest } from "../inngest/client.js";
 import { functions } from "../inngest/functions.js";
 
-// Create the handler
 export default serve({
   client: inngest,
   functions,
