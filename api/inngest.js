f027abb75e1f5aa37e499d8d51fe0d835dd02584
nagadarshan08 <darshanaravn.855rc@gmail.com>
2026-04-23 09:55:30 +0530

    added clerk sync functions

--- api/inngest.js
+++ api/inngest.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/express";
 import { inngest } from "../inngest/client.js";
-import { functions } from "../inngest/functions.js";
+import { functions } from "../../inngest/functions.js";
 
 export default serve({
   client: inngest,
