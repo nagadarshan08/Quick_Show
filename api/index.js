33276826b58445bf67963db918d9a286c0a22fce
nagadarshan08 <darshanaravn.855rc@gmail.com>
2026-04-23 09:40:34 +0530

    fix routing and imports

--- api/index.js
+++ api/index.js
@@ -1,36 +1,15 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-
-import connectDB from "../config/db.js";
-import userRoutes from "../routes/userRoutes.js";
-
-import { serve } from "inngest/express";
-import { inngest } from "../inngest/client.js";
-import functions from "../inngest/functions.js";
-
-dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-await connectDB();
-
-app.use("/api/users", userRoutes);
-
-// ✅ ADD THIS
-app.use(
-  "/api/inngest",
-  serve({
-    client: inngest,
-    functions,
-  })
-);
-
 app.get("/", (req, res) => {
-  res.send("API running...");
+  res.send("API is working 🚀");
 });
+
+// ❌ REMOVE any inngest/functions import from here
 
 export default app;
