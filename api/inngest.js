a9cf7d346bb82c775d6eb1965675d0fae296315f
nagadarshan08 <darshanaravn.855rc@gmail.com>
2026-04-24 14:31:46 +0530

    fix: use vercel adapter

--- api/inngest.js
+++ api/inngest.js
@@ -1,4 +1,4 @@
-import { serve } from "inngest/express";
+import { serve } from "inngest/vercel";  // ✅ FIXED
 import { inngest } from "../inngest/client.js";
 import functions from "../inngest/functions.js";
 
