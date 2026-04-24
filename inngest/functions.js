55ed49f3874693c0db1f15612dc993abc28e6190
nagadarshan08 <darshanaravn.855rc@gmail.com>
2026-04-24 16:02:59 +0530

    fix mongo auth

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,47 +1,23 @@
-import { inngest } from "./client.js";
-import User from "../models/User.js";
-import connectDB from "../config/db.js";
-
 const syncUser = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    await connectDB();
+    try {
+      await connectDB();
+      console.log("Connected to DB");
 
-    const { id, email_addresses, first_name, last_name } = event.data;
+      console.log("Event data:", event.data);
 
-    await User.create({
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      firstName: first_name,
-      lastName: last_name,
-    });
+      const user = await User.create({
+        clerkId: event.data.id,
+        email: event.data.email_addresses?.[0]?.email_address,
+        firstName: event.data.first_name,
+        lastName: event.data.last_name,
+      });
+
+      console.log("User saved:", user);
+    } catch (err) {
+      console.error("ERROR:", err);
+    }
   }
 );
-
-const updateUser = inngest.createFunction(
-  { id: "update-user-from-clerk" },
-  { event: "clerk/user.updated" },
-  async ({ event }) => {
-    await connectDB();
-    const { id, first_name, last_name } = event.data;
-
-    await User.findOneAndUpdate(
-      { clerkId: id },
-      { firstName: first_name, lastName: last_name }
-    );
-  }
-);
-
-const deleteUser = inngest.createFunction(
-  { id: "delete-user-from-clerk" },
-  { event: "clerk/user.deleted" },
-  async ({ event }) => {
-    await connectDB();
-    const { id } = event.data;
-
-    await User.findOneAndDelete({ clerkId: id });
-  }
-);
-
-export const functions = [syncUser, updateUser, deleteUser];
