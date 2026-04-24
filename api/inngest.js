48417ae977fafbc314c7458c41b52ecc99195e3a
nagadarshan08 <darshanaravn.855rc@gmail.com>
2026-04-24 14:28:13 +0530

    final fix

--- api/inngest.js
+++ api/inngest.js
@@ -2,9 +2,7 @@ import { serve } from "inngest/express";
 import { inngest } from "../inngest/client.js";
 import functions from "../inngest/functions.js";
 
-const handler = serve({
+export default serve({
   client: inngest,
   functions,
 });
-
-export default handler;
